8f11a3edd3b132323b85b44e7fdbe574bc00506d
yikai <15738800415@163.com>
2021-12-15 10:30:27 +0800

    提供注释

--- app/src/main/java/com/hanshow/godemo/MainActivity.java
+++ app/src/main/java/com/hanshow/godemo/MainActivity.java
@@ -16,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
         //这里调用通过jni调用go，然后go调用MainActivity 的showToast，展示出Toast的message
         findViewById(R.id.button).setOnClickListener(view -> {
+            // 适配 Android Q
             Seq.setContext(MainActivity.this);
+            // 调用Go代码的方法， 在go代码中通过jni回调public void showToast(String message) 这个方法
             CallGo.showToast();
         });
     }
